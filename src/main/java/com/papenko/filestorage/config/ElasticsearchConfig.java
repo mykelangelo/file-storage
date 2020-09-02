b90532c298267f70500a9cf452fb6dbd67d1cb68
reactive_cat <mykola.papenko@gmail.com>
2020-09-02 18:19:51 +0300

    [80] add logging to ElasticsearchConfig

--- src/main/java/com/papenko/filestorage/config/ElasticsearchConfig.java
+++ src/main/java/com/papenko/filestorage/config/ElasticsearchConfig.java
@@ -1,6 +1,8 @@
 package com.papenko.filestorage.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -11,6 +13,8 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.papenko.filestorage.repository")
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ElasticsearchConfig.class);
+    
     @Value("#{systemEnvironment['ELASTIC_HOST_AND_PORT']}")
     private String elasticHostAndPort;
 
@@ -20,8 +24,10 @@ public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
     @Override
     public RestHighLevelClient elasticsearchClient() {
         if ("production".equals(profile)) {
+            LOGGER.info("Using production environment");
             return RestClients.create(ClientConfiguration.create(elasticHostAndPort)).rest();
         }
+        LOGGER.info("Using default local environment");
         return RestClients.create(ClientConfiguration.localhost()).rest();
     }
 }
