f248d82b33458bd56a751af59b0b1024fcc1bac5
reactive_cat <mykola.papenko@gmail.com>
2020-09-02 17:20:14 +0300

    [79,80] add Procfile, differentiate between production and local environments using environment variable

---
+++ Procfile
@@ -0,0 +1,2 @@
+release:
+web: java -Dserver.port=14224 $JAVA_OPTS -jar target/file-storage-0.0.1-SNAPSHOT.jar
--- src/main/java/com/papenko/filestorage/config/ElasticsearchConfig.java
+++ src/main/java/com/papenko/filestorage/config/ElasticsearchConfig.java
@@ -1,6 +1,7 @@
 package com.papenko.filestorage.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -10,8 +11,16 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.papenko.filestorage.repository")
 public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+    private static final String bonsaiHostAndPort = "pepper-121090209.us-east-1.bonsaisearch.net:443";
+
+    @Value("${SPRING_PROFILES_ACTIVE}")
+    private String profile;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
+        if ("production".equals(profile)) {
+            return RestClients.create(ClientConfiguration.create(bonsaiHostAndPort)).rest();
+        }
         return RestClients.create(ClientConfiguration.localhost()).rest();
     }
 }
